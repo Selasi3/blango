84a0d201b2e57507d9f06debba0bd35b356f516b
sela tech <selatechgh@gmail.com>
2022-10-25 10:18:30 +0000

    Finish generic relationhips

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 
 
@@ -9,4 +9,5 @@ class PostAdmin(admin.ModelAdmin):
   
 # Register your models here.
 admin.site.register(Tag)
+admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
