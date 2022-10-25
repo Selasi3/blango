3c75f4e950faf07aeda68e5e16676618136a2b4d
sela tech <selatechgh@gmail.com>
2022-10-25 13:06:50 +0000

    added index page

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,15 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
-
+class Comment(models.Model):
+  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  content = models.TextField()
+  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey("content_type", "object_id")
+  created_at = models.DateTimeField(auto_now_add=True)
+  modified_at = models.DateTimeField(auto_now=True)
+  
 class Tag(models.Model):
   value = models.TextField(max_length=100)
 
@@ -26,12 +34,5 @@ class Post(models.Model):
   def __str__(self):
       return self.title
 
-class Comment(models.Model):
-  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  content = models.TextField()
-  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-  object_id = models.PositiveIntegerField()
-  content_object = GenericForeignKey("content_type", "object_id")
-  created_at = models.DateTimeField(auto_now_add=True)
-  modified_at = models.DateTimeField(auto_now=True)
+
     
